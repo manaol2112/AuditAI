add333811338d44cf595fdb8221e871074b99860
manaol2112 <167021108+manaol2112@users.noreply.github.com>
2024-06-15 14:48:41 -0700

    6.15.2024

--- AUDITAI/settings.py
+++ AUDITAI/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-OPEN_AI_KEY = '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'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
